fd072dd81abccddc3dbae706b60035ad1409606a
Benjamin Holmes <benjamin.holmes@gmail.com>
2022-08-18 16:13:55 +0100

    updates for usability

--- main.tf
+++ main.tf
@@ -1,13 +1,10 @@
 terraform {
-  
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.18.0"
     }
   }
-
-  required_version = ">= 1.2.7"
 }
 
 provider "azurerm" {
@@ -31,37 +28,37 @@ data "azurerm_subnet" "compute_sn" {
 
 resource "azurerm_public_ip" "rhel" {
   name                = format("%s-%s", var.vm_name, "public") 
-  resource_group_name = azurerm_resource_group.compute_rg.name
-  location            = azurerm_resource_group.compute_rg.location
+  resource_group_name = data.azurerm_resource_group.compute_rg.name
+  location            = data.azurerm_resource_group.compute_rg.location
   allocation_method   = "Dynamic"
 }
 
 resource "azurerm_network_interface" "rhel" {
   name                = format("%s-%s", var.vm_name, "if") 
-  location            = azurerm_resource_group.compute_rg.location
-  resource_group_name = azurerm_resource_group.compute_rg.name
+  location            = data.azurerm_resource_group.compute_rg.location
+  resource_group_name = data.azurerm_resource_group.compute_rg.name
 
   ip_configuration {
     name                          = format("%s-%s", var.vm_name, "internal") 
-    subnet_id                     = azurerm_subnet.rhel.id
+    subnet_id                     = data.azurerm_subnet.compute_sn.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = "azurerm_public_ip.rhel.id"
+    public_ip_address_id          = azurerm_public_ip.rhel.id
   }
 }
 
 resource "azurerm_linux_virtual_machine" "rhel" {
   name                = var.vm_name
-  resource_group_name = azurerm_resource_group.rhel.name
-  location            = azurerm_resource_group.rhel.location
+  resource_group_name = data.azurerm_resource_group.compute_rg.name
+  location            = data.azurerm_resource_group.compute_rg.location
   size                = var.vm_size
-  admin_username      = "azureuser"
+  admin_username      = var.ssh_admin_user
   network_interface_ids = [
     azurerm_network_interface.rhel.id,
   ]
 
   admin_ssh_key {
-    username   = "azureuser"
-    public_key = var.ssh_public_key
+    username   = var.ssh_admin_user
+    public_key = var.ssh_admin_user_public_key
   }
 
   os_disk {
@@ -72,9 +69,9 @@ resource "azurerm_linux_virtual_machine" "rhel" {
   source_image_reference {
     publisher = "RedHat"
     offer     = "RHEL"
-    sku       = "90-gen2"
+    sku       = var.vm_sku
     version   = "latest"
   }
 
-  # tags = azurerm_resource_group.rhel.tags
+  tags = var.vm_tags
 }
--- variables.tf
+++ variables.tf
@@ -1,27 +1,46 @@
+variable "vm_name" {
+  description = "Azure Virtual Machine name"
+  type        = string
+}
+
 variable "vm_size" {
   description = "Azure Virtual Machine size"
   default     = "Standard_D2as_v4"
   type        = string
 }
 
-variable "vm_name" {
-  description = "Azure Virtual Machine name"
+variable "vm_sku" {
+  description = "Azure RHEL Virtual Machine SKU"
+  default     = "90-gen2"
   type        = string
-}
+} 
+
+variable "vm_tags" {
+  description = "Azure Virtual Machine SKU"
+  default     = {}
+  type = map
+} 
 
 variable "rg_name" {
-  description = "Resource Group name"
+  description = "Resource Group Name"
   default     = "terraformDefaultRG"
   type        = string
 }
 
 variable "location" {
-  description = "Location"
+  description = "Resource Location"
   default     = "uksouth"
   type        = string
 }
 
-variable "ssh_public_key" {
-  description = "Admin User SSH Key"
+variable "ssh_admin_user" {
+  description = "Admin User SSH Username"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "ssh_admin_user_public_key" {
+  description = "Admin User SSH Public Key"
   type        = string
 }
+
